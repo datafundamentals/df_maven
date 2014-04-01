8e70b7c9d1f20845ba3d32effcaa961137ca9791
jcarapet <jeffcarapetyan@gmail.com>
2014-04-01 16:16:59 -0500

    added path information to maven.sh to ACTUALLY set path

--- recipes/set_maven_home.rb
+++ recipes/set_maven_home.rb
@@ -2,7 +2,7 @@ directory "/etc/profile.d" do
 	mode "0755" 
 end
 file "/etc/profile.d/maven.sh" do 
-	content "export MAVEN_HOME=/usr/local/maven/apache-maven-3.1.1"
+	content "export MAVEN_HOME=/usr/local/maven/apache-maven-3.1.1 \n export PATH=$PATH:$MAVEN_HOME/bin"
 	owner "root" 
 	action :create
 end
